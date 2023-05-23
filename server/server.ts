1a521b140fb573e841e4a87c8d41f340061f30a5
Nyali.ce <nathan@alconat.ca>
2023-05-23 10:31:20 -0400

    added react router in server

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('../client/dist'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const clientBuildPath = path.resolve(__dirname, '../../client/dist');
+
+app.use(express.static(clientBuildPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
